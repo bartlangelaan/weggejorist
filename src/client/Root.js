56e4faf677497bc77ee36c38a9520c87ca0c055a
Bart Langelaan <langelaanbart@gmail.com>
2017-01-14 16:12:22 +0100

    fix routing

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,13 +1,14 @@
-
 import React from 'react';
-import Router from 'react-router/lib/Router';
-import browserHistory from 'react-router/lib/browserHistory';
+import { Router, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 import routes from '../routes';
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 // We need a Root component for React Hot Loading.
 function Root() {
   return (
-    <Router history={browserHistory} routes={routes} />
+    <Router history={appHistory} routes={routes} />
   );
 }
 
