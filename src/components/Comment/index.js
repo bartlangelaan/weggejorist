84ef785e01976111dcfbb9148c20cec52bc903c4
Bart Langelaan <langelaanbart@gmail.com>
2017-03-04 20:12:34 +0100

    Fix overflowing text

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -10,14 +10,16 @@ export default function (comment) {
   const date = new Date(comment.published);
   const timeString = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
   const dateString = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
-  return (<div style={{ margin: 20 }}><Card>
+  return (<div style={{ margin: 20, wordWrap: 'break-word' }}><Card>
     <CardHeader
       title={comment.user}
       subtitle={`Geplaatst ${dateString} ${timeString} op ${comment.videoId.title}`}
       avatar={<Avatar>{comment.user.substr(0, 1).toUpperCase()}</Avatar>}
     />
     <CardText>
-      {comment.content}
+      <div>
+        {comment.content}
+      </div>
     </CardText>
   </Card></div>);
 }
