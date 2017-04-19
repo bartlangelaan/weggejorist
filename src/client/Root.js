29c6557b3071a9845cc96e61354053c73e2ad86b
Bart Langelaan <langelaanbart@gmail.com>
2017-04-19 17:33:06 +0200

    Move Google Analytics to React Router

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Router, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
+import ReactGA from 'react-ga';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import routes from '../routes';
+
+ReactGA.initialize('UA-97629232-1');
+
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
@@ -10,7 +14,13 @@ const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 function Root() {
   return (
     <MuiThemeProvider>
-      <Router history={appHistory} routes={routes} />
+      <Router
+        history={appHistory}
+        routes={routes}
+        onUpdate={() => {
+          ReactGA.pageview(`/${location.hash}`);
+        }}
+      />
     </MuiThemeProvider>
   );
 }
